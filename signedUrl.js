c3fe72c5fac9ea130878bfa409a461e5052ae9e0
Edgar García <edgar.garcia@gmail.com>
2020-01-21 11:22:02 -0400

    First commit

---
+++ signedUrl.js
@@ -0,0 +1,10 @@
+var AWS = require("aws-sdk")
+
+const s3 = new AWS.S3({
+    signatureVersion: 'v4'
+});
+
+const params = {Bucket: 'signedurl-nodejs-test', Key: 'Edgar Garcia resume.pdf'}
+const url = s3.getSignedUrl('getObject', params);
+
+console.log("The Signed URL is: " + url)
